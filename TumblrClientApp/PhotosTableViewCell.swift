d6d616166767fee80e86d0281f606e8dbd9897c3
A <Maker@As-iMac-46.local>
2017-01-31 20:20:50 -0500

    done

---
+++ TumblrClientApp/PhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosTableViewCell.swift
+//  TumblrClientApp
+//
+//  Created by A on 1/31/17.
+//  Copyright © 2017 SVM. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
